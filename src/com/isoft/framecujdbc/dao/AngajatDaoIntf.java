2901422e585ed5193c44cb5b1a4c9ec946e862d9
Code <javasetraining@gmail.com>
2016-08-29 03:40:02 +0300

    initial commit :)

--- src/com/isoft/framecujdbc/dao/AngajatDaoIntf.java
+++ src/com/isoft/framecujdbc/dao/AngajatDaoIntf.java
@@ -4,27 +4,24 @@ import com.isoft.framecujdbc.domain.Angajat;
 
 import java.util.List;
 
-/**
- *
- * @author iurasun
- */
+
 public interface AngajatDaoIntf {
 
-    
+
     List<Angajat> findAll(Angajat angajat) throws Exception;
-    
+
     //CRUD  
-    
+
     List<Angajat> findAll() throws Exception;
     //Angajat findById(int id)throws Exception;
 
-    public void save(Angajat readForm);
+    void save(Angajat readForm);
 
-    public Angajat findById(int id) throws Exception;
+    Angajat findById(int id) throws Exception;
 
-    public void update(Angajat angajat)throws Exception;
+    void update(Angajat angajat) throws Exception;
 
-    public void delete(Angajat angajat)throws Exception;
-    
+    void delete(Angajat angajat) throws Exception;
+
 }
 
